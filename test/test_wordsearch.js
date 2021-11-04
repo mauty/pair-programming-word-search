9446d19b37ec8d1f25d76b43188d75496b6853f1
Mike Auty <m@auty.ca>
2021-11-04 01:10:01 +0000

    made better

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,51 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  it("should return false if the word is not present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'N', 'E', 'A', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'Y', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ARRAY')
+
+    assert.isTrue(result);
+  });
+
+
+  it("should return false if array is empty", function() {
+    const result = wordSearch([], 'ARRAY')
+
+    assert.isFalse(result);
+  });
+
+
+
+  //ADDIONAL TEST IDEAS
+  //test if matrix length < word length
+  //test if not numbers
+  //test upper or lower casee 
+  //vert true
 });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const log = console.log
 
-module.exports = wordSearch
+const transpose = function(matrix) {
+    let transposed = [];
+    for(let row = 0; row < matrix.length; row++){
+      for(let col = 0; col < matrix[row].length; col++) {
+        if(transposed[col] === undefined) {
+          transposed[col] = [];
+        }
+        transposed[col][row] = matrix[row][col];
+      }
+    }
+    //console.log('this is transposed: ', transposed);
+    return transposed;
+  };
+
+
+
+//console.log('outputting');
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''))
+    const transposedMatrix = transpose(letters)
+    const verticalJoin = transposedMatrix.map(js => js.join(''))
+    let doesWordExist = false;
+    for (char of horizontalJoin) {   
+        if (char.includes(word)) {
+            doesWordExist = true;
+        }
+    }
+    for (char2 of verticalJoin) {
+        if (char2.includes(word)) {
+            doesWordExist = true;
+        }
+    }
+return doesWordExist;
+};
+
+
+module.exports = wordSearch;
